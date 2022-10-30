a93f169db2815146d78dc5ae2a3cfc49b8b61a05
Fer Equis <86884315+FerEquis@users.noreply.github.com>
2022-10-30 14:42:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function validarForm() {
+    let x = document.forms["myform"]["email"].value;
+    if (x == "") {
+      alert("Para contactarte necesitamos por lo menos un email");
+      return false;
+    }
+  }
+
+
+function traer() {
+  fetch('https://randomuser.me/api')
+    .then(res => res.json())
+    .then(res => {
+        console.log(res)
+        console.log(res.results[0].email)
+        contenido.innerHTML= `
+          <img src="${res.results[0].picture.large}
+           "width="300px" 
+           class="img-fluid rounded-circle">
+          <p>Nombre: ${res.results[0].name.first}</p>
+          <p>Mail: ${res.results[0].email}</p>`
+    })
+  }
